f007e6a55bd5db2a8a0181cbe7f17efd55aa976f
Paulo Sérgio <sergiomartinsbsb@gmail.com>
2017-09-01 21:54:37 -0300

    Autenticação

--- app/src/main/java/br/iesb/androidiesb/LoginActivity.java
+++ app/src/main/java/br/iesb/androidiesb/LoginActivity.java
@@ -1,40 +1,91 @@
 package br.iesb.androidiesb;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEmail;
+    private EditText loginSenha;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_login);
 
-        Button b = (Button) findViewById(R.id.btnCadastrar);
+        loginEmail = (EditText) findViewById(R.id.loginEmail);
+        loginSenha = (EditText) findViewById(R.id.loginSenha);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (loginEmail.getText() == null || loginEmail.getText().toString().equals("")) {
+                    loginEmail.setError("Digite o seu email!");
+                    return;
+                }
+
+                if (loginSenha.getText() == null || loginSenha.getText().toString().equals("")) {
+                    loginSenha.setError("Digite sua senha!");
+                    return;
+                }
+
+
+                mAuth.signInWithEmailAndPassword(loginEmail.getText().toString(), loginSenha.getText().toString())
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Erro no login", Toast.LENGTH_LONG).show();
+                                } else {
+                                    FirebaseUser user = task.getResult().getUser();
+                                    if (user != null) {
+                                        Intent it = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(it);
+                                        finish();
+
+                                    }
+                                }
+                            }
+                        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder (GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+
+        Button btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
         View.OnClickListener x = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent (LoginActivity.this, CadastroActivity.class));
+                startActivity(new Intent(LoginActivity.this, CadastroActivity.class));
             }
         };
-        b.setOnClickListener(x);
-    }
+        btnCadastrar.setOnClickListener(x);
 
+            }
 
-    /* BTNCADASTRAR
-    public void mostrarTelaCadastro(View parametro){
-        Intent it = new Intent(this, MainActivity.class);
-        startActivity(it);
-    }
-    */
-
-    public void efetuarLogin(View parametro){
-        Intent it = new Intent(this, TelaPrincipalActivity.class);
-        startActivity(it);
-    }
+        })
+    ;}
 }
